acbe2fe05c9522bf6aa44209feea93f955080a47
shaishab316 <shaishabchandrashil@gmail.com>
2026-03-09 14:39:48 +0600

    feat: integrate Swagger and Scalar UI for API documentation

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,62 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import type { Env } from './config/app.config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = app.get(ConfigService<Env, true>);
+
+  //? Swagger setup
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Todo API')
+    .setDescription(
+      `This API allows you to manage your todo items. You can create, read, update, and delete todos. Each todo has a title, description, and a completed status.`,
+    )
+    .setVersion(process.env.npm_package_version ?? '1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerUiEnabled: false, //? Disable Swagger UI since we're using Scalar UI
+  });
+
+  // Scalar UI at /docs
+  app.use(
+    '/docs',
+    apiReference({
+      content: document,
+      layout: 'modern',
+      theme: 'kepler',
+      hideClientButton: true,
+      showOperationId: true,
+      showSidebar: true,
+      showDeveloperTools: 'never',
+      showToolbar: 'localhost',
+      operationTitleSource: 'summary',
+      persistAuth: true,
+      telemetry: true,
+      isEditable: false,
+      isLoading: false,
+      documentDownloadType: 'both',
+      hideSearch: false,
+      withDefaultFonts: true,
+      defaultOpenFirstTag: true,
+      defaultOpenAllTags: true,
+      expandAllModelSections: false,
+      expandAllResponses: false,
+      orderSchemaPropertiesBy: 'alpha',
+      orderRequiredPropertiesFirst: true,
+      _integration: 'nestjs',
+      default: false,
+    }),
+  );
+
   //? Enable shutdown hooks to allow graceful shutdown of the application
   app.enableShutdownHooks();
-
-  const config = app.get(ConfigService<Env, true>);
 
   await app.listen(config.get('PORT', { infer: true }));
 
